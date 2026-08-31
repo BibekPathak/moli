33aa7059191157b1bcc24e777507dd8794cc38a0
BibekPathak <bibekpathak101@gmail.com>
2026-08-31 16:36:53 +0530

    embed fonts and an invisible text layer in printToPDF

--- moli-renderer-v8/src/lib.rs
+++ moli-renderer-v8/src/lib.rs
@@ -276,6 +276,7 @@ pub use runtime::{
     RendererPageDiagnosticsSnapshot, RendererPageDumpFormat, RendererPageDumpOptions,
     RendererPageDumpStripOptions, RendererPageHandle, RendererPageReply,
     RendererPageReservationToken, RendererPageState, RendererPageTestingHandle, RendererPageView,
+    RendererPdfFont, RendererPdfGlyph, RendererPdfTextLayer, RendererPdfTextRun,
     RendererPendingDownloadActivation, RendererPendingDownloadResponse,
     RendererPendingFileChooserActivation, RendererPendingJavaScriptDialog,
     RendererPendingPopupActivation, RendererPendingSameDocumentNavigation,
